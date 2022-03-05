91f12148c676c03c12b9019d80d91482f3831fc7
Alex Ballmer <gnu3ra@riseup.net>
2022-03-04 22:49:16 -0600

    entities and empty migrations

--- src/persist/mod.rs
+++ src/persist/mod.rs
@@ -4,6 +4,8 @@ pub(crate) type Result<T> = anyhow::Result<T>;
 
 pub(crate) mod redis;
 
+pub(crate) mod entity;
+
 #[async_trait]
 pub trait DbTable<T> {
     async fn insert(&self, pool: &T, wrapper: Option<String>) -> Result<()>;
