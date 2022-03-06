4b008d577c6fe50ee5cac5e8779c2e30a5c76fe5
Alex Ballmer <gnu3ra@riseup.net>
2022-03-06 02:20:21 -0600

    remove unused DbTable trait

--- bobot_impl/src/persist/mod.rs
+++ bobot_impl/src/persist/mod.rs
@@ -1,11 +1,4 @@
-use async_trait::async_trait;
-
 pub(crate) type Result<T> = anyhow::Result<T>;
 
 #[allow(dead_code)]
 pub(crate) mod redis;
-
-#[async_trait]
-pub trait DbTable<T> {
-    async fn insert(&self, pool: &T, wrapper: Option<String>) -> Result<()>;
-}
