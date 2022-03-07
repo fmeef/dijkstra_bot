0d2e5f832ebb6b062a55e966797852340d225d0e
Alex Ballmer <gnu3ra@riseup.net>
2022-03-06 19:56:39 -0500

    add module MigratorTraits to migration crate

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -8,6 +8,10 @@ pub struct Migrator;
 #[async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20220101_000001_create_table::Migration)]
+        let mut module_migrations = bobot_impl::modules::get_migrations();
+        let mut core_migrations: Vec<Box<dyn MigrationTrait>> =
+            vec![Box::new(m20220101_000001_create_table::Migration)];
+        core_migrations.append(&mut module_migrations);
+        core_migrations
     }
 }
