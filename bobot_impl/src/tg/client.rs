8731f06384b261d85860ccd66f796a11d544bd30
Alex Ballmer <gnu3ra@riseup.net>
2022-03-07 00:52:38 -0600

    unfinished base for telegram client

---
+++ bobot_impl/src/tg/client.rs
@@ -0,0 +1,70 @@
+use grammers_client::{Client, Config, InitParams, Update};
+use grammers_session::Session;
+use log::debug;
+
+use super::Result;
+
+pub struct TgClient {
+    client: Client,
+}
+
+impl TgClient {
+    pub async fn connect<T>(token: T, api_id: i32, api_hash: T, session: T) -> Result<Self>
+    where
+        T: Into<String>,
+    {
+        let token: String = token.into();
+        let api_hash: String = api_hash.into();
+        let session: String = session.into();
+        let mut res = Self {
+            client: Client::connect(Config {
+                api_id,
+                api_hash: api_hash.clone().into(),
+                session: Session::load_file_or_create(session.clone())?,
+                params: InitParams {
+                    catch_up: true,
+                    ..Default::default()
+                },
+            })
+            .await?,
+        };
+
+        if !res.client.is_authorized().await? {
+            res.client
+                .bot_sign_in(token.as_ref(), api_id, api_hash.as_ref())
+                .await?;
+            res.client.session().save_to_file(session)?;
+        }
+
+        Ok(res)
+    }
+
+    pub async fn run(self) -> Result<()> {
+        let r = while let Some(update) = tokio::select! {
+            _ = tokio::signal::ctrl_c() => Ok(None),
+            result = self.client.next_update() => result,
+        }? {
+            let c = self.clone();
+            tokio::spawn(async move {
+                if let Err(err) = c.single_update(update).await {
+                    debug!("failed to handle update {}", err);
+                }
+            });
+        };
+
+        Ok(r)
+    }
+
+    async fn single_update(self, update: Update) -> Result<()> {
+        crate::modules::process_updates(self, update).await;
+        Ok(())
+    }
+}
+
+impl Clone for TgClient {
+    fn clone(&self) -> Self {
+        TgClient {
+            client: self.client.clone(),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,29 @@
 #![allow(dead_code)]
 use std::env;
 
+const SESSION_FILE: &str = "/tmp/whatever.session"; //TODO: change this
+
 async fn async_main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let _token = env::var("FMEFTOKEN").expect("need to set telegram bot token");
+    let _api_id: i32 = env::var("API_ID")
+        .expect("need to set API_ID")
+        .parse()
+        .expect("invalid API_ID");
+    let _api_hash = env::var("API_HASH").expect("need to set API_HASH");
     let _pg_connection_str =
         env::var("PG_CONNECTION_PROD").expect("need to set PG_CONNECTION_PROD");
     let _redis_connection_str =
         env::var("REDIS_CONNECTION_PROD").expect("need to set REDIS_CONNECTION_PROD");
 
+    let client = bobot_impl::tg::client::TgClient::connect(
+        _token,
+        _api_id,
+        _api_hash,
+        SESSION_FILE.to_string(),
+    )
+    .await?;
+
+    client.run().await?;
     Ok(())
 }
 
